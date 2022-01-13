55046e8e7e32ff906b09852b3e756189c544803b
Hizkyas Tekle <hizkyastekle@gmail.com>
2022-01-13 14:45:10 +0300

    type annotated everything

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,17 +3,17 @@
 logging with sensitive information redaction
 """
 
-from typing import List
+from typing import List, Tuple
 import re
 import logging
 from mysql.connector import connection
 from os import getenv
 
 
-PII_FIELDS = ("ssn", "password", "name", "email", "phone")
+PII_FIELDS: Tuple[str] = ("ssn", "password", "name", "email", "phone")
 
 
-def filter_datum(fields: List[str], blur: str, msg: str, sep: str) -> None:
+def filter_datum(fields: List[str], blur: str, msg: str, sep: str) -> str:
     """ redact sensetive info """
     blurred: List[str] = [re.sub(r'(.+=)(.+)', r'\1' + blur, pair)
                           if pair.split('=')[0] in fields
@@ -25,12 +25,12 @@ def get_logger() -> logging.Logger:
     """
     get a logger object
     """
-    formatter = RedactingFormatter(PII_FIELDS)
+    formatter: logging.Formatter = RedactingFormatter(PII_FIELDS)
 
-    handler = logging.StreamHandler()
+    handler: logging.Handler = logging.StreamHandler()
     handler.setFormatter(formatter)
 
-    my_logger = logging.getLogger("user_data")
+    my_logger: logging.Logger = logging.getLogger("user_data")
     my_logger.setLevel(logging.INFO)
     my_logger.propagate = False
     my_logger.addHandler(handler)
@@ -42,10 +42,10 @@ def get_db() -> connection.MySQLConnection:
     """
     get a connection to a db
     """
-    usr_name = getenv("PERSONAL_DATA_DB_USERNAME")
-    usr_pass = getenv("PERSONAL_DATA_DB_PASSWORD")
-    db_host = getenv("PERSONAL_DATA_DB_HOST")
-    db_name = getenv("PERSONAL_DATA_DB_NAME")
+    usr_name: str = getenv("PERSONAL_DATA_DB_USERNAME")
+    usr_pass: str = getenv("PERSONAL_DATA_DB_PASSWORD")
+    db_host: str = getenv("PERSONAL_DATA_DB_HOST")
+    db_name: str = getenv("PERSONAL_DATA_DB_NAME")
     return connection.MySQLConnection(
         user=usr_name,
         password=usr_pass,
@@ -57,22 +57,22 @@ class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
 
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+    REDACTION: str = "***"
+    FORMAT: str = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR: str = ";"
 
     def __init__(self, fields: List[str]):
         """
         initialization
         """
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
+        self.fields: List[str] = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """
         redact message and format log
         """
-        filtered_message = filter_datum(
+        filtered_message: str = filter_datum(
             self.fields,
             self.REDACTION,
             record.getMessage(),
@@ -85,11 +85,11 @@ def main() -> None:
     """
     Entry point
     """
-    my_connection = get_db()
-    my_cursor = my_connection.cursor()
+    my_connection: connection.MySQLConnection = get_db()
+    my_cursor: connection.MySQLConnection.cursor = my_connection.cursor()
     my_cursor.execute("SELECT * FROM users;")
 
-    my_logger = get_logger()
+    my_logger: logging.Logger = get_logger()
     for row in my_cursor:
         row = map(str, row)
         temp = zip(
@@ -102,7 +102,7 @@ def main() -> None:
              "last_login",
              "user_agent"),
             row)
-        message = ";".join(["=".join(pair) for pair in temp])
+        message: str = ";".join(["=".join(pair) for pair in temp])
         my_logger.info(message)
 
 
