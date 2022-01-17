7104831b02387fd06f7ff2f0ce8ba93b85c01879
Hizkyas Tekle <hizkyastekle@gmail.com>
2022-01-17 15:49:23 +0300

    first 10 tasks complete

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+module for main flask app
+"""
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    Entry point
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+Module for Authentication
+"""
+
+
+from lib2to3.pgen2.pgen import generate_grammar
+from db import DB
+from user import User
+import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    encrypts a password
+    """
+    return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """
+    generate unique id
+    """
+    return str(uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """
+        initialization
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        register new user
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_pass = _hash_password(password)
+            return self._db.add_user(email, hashed_pass)
+        raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email, password) -> bool:
+        """
+        validates user password
+        """
+        try:
+            usr = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                bytes(
+                    password,
+                    'utf-8'),
+                usr.hashed_password)
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """
+        create a new session
+        """
+        try:
+            usr = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(usr.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
